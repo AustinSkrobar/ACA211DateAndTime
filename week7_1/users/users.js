b3327289dd0873d09abae254db3633c08aaac534
Austin Skrobarczyk <austin.skrobar@gmail.com>
2020-09-20 16:56:19 -0500

    updated api practice to hide address

--- week7_1/users/users.js
+++ week7_1/users/users.js
@@ -1,7 +1,7 @@
 console.log("loading in the user.js file");
 
 console.log("About to make a fetch call");
-let fetchPromise = window.fetch("https://randomuser.me/api/");
+let fetchPromise = window.fetch("https://randomuser.me/api/?results=5");
 
 let jsonPromise = fetchPromise.then(function (data) {
   console.log("Fetch call finished ", data);
@@ -18,7 +18,7 @@ function process(users) {
   let ul = document.getElementById("people");
 
   users.forEach(function (user) {
-    console.log("users first name: ", user.name.first);
+    console.log("users  name: ", user.name.first + " " + user.name.last);
 
     let li = document.createElement("li");
     ul.appendChild(li);
@@ -36,16 +36,20 @@ function process(users) {
     addressDiv.innerHTML =
       user.location.street.number + " " + user.location.street.name;
     // sets the display to empty
-    addressDiv.style.display = "";
+    addressDiv.style.visibility = "hidden";
     //if display is empty we want to not show address
-    if (addressDiv.style.display == "") {
-      displayValue = "none";
-
-      addressDiv.style.display = displayValue;
-    }
+    
+    // let displayValue = "none";
+    
+    // addressDiv.style.display = displayValue;
     //on click change the display from none to block so it can show the address when name is clicked
     nameDiv.addEventListener("click", function () {
-      addressDiv.style.display = "block";
+     if( addressDiv.style.visibility == "hidden"){
+       addressDiv.style.visibility = "visible";
+     }else{
+       console.log("the address should be hidden now")
+      addressDiv.style.visibility = "hidden";
+     }
     });
   });
 }
